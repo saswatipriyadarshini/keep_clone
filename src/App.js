cd37067962ad3dd29cd357b106747eba2a590aca
saswati priyadarshini <saswati.p13@gmail.com>
2020-09-23 21:55:14 +0530

    sidebar component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {NavBar} from "NavBar"
+import {NavBar} from "NavBar";
+import {SideBar} from "SideBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <SideBar />
     </div>
   );
 }
--- src/NavBar/components/NavBar.js
+++ src/NavBar/components/NavBar.js
@@ -1,9 +1,52 @@
 import React, {Component} from "react";
+import "./index.css";
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem, NavLink } from 'reactstrap';
+import { AppstoreOutlined, MenuOutlined, SearchOutlined, UndoOutlined, SettingOutlined, BorderBottomOutlined } from '@ant-design/icons';
+import {Input} from 'antd';
+import 'antd/dist/antd.css';
 
 export default class NavBar extends Component{
   render() {
     return(
-      <>hello</>
+      <>
+        <div className='kc-navbar__container'>
+          <Navbar color="light" light expand="md">
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                <NavItem>
+                  <MenuOutlined/>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">
+                    <img src="https://www.gstatic.com/images/branding/product/1x/keep_48dp.png" alt=""/>Keep
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <Nav className='m-auto'>
+                {/*<NavItem>*/}
+                  <Input prefix={<SearchOutlined />} />
+                {/*</NavItem>*/}
+              </Nav>
+              <Nav className='ml-auto'>
+                <NavItem>
+                  <UndoOutlined />
+                </NavItem>
+                <NavItem>
+                  <AppstoreOutlined className='kc-navbar-right-items'/>
+                {/*  <PauseOutlined />*/}
+                </NavItem>
+                <NavItem>
+                  <SettingOutlined />
+                </NavItem>
+                <NavItem>
+                  <BorderBottomOutlined />
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      </>
     )
   }
 }
